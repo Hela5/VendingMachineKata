afb702bd0420ad0f0bcdf147d39dc1151274c7ce
Andria <andria.fuquen@gmail.com>
2018-12-02 15:59:30 -0500

    refactored CoinDAOImplTest to include Mockito

--- src/test/java/VendingMachine/CoinDAOImplTest.java
+++ src/test/java/VendingMachine/CoinDAOImplTest.java
@@ -1,44 +1,56 @@
 package VendingMachine;
 
+import com.VendingMachine.*;
 import com.VendingMachine.Coin;
 import com.VendingMachine.CoinDAOImpl;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoFramework.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CoinDAOImplTest {
 
         @Test
-        public void getListOfValidCoins(){
-            Coin[] testCoin;
-            testCoin = Coin.values();
-            CoinDAOImpl testList = new CoinDAOImpl();
-            assertArrayEquals(testCoin, testList.getCoinTypes());
+        public void testGetListOfValidCoins(){
+            Coin[] testCoin = Coin.values();
+            CoinDAOImpl testList = mock(CoinDAOImpl.class);
+            when(testList.getCoinTypes()).thenReturn(testCoin);
+            assertEquals(testCoin, testList.getCoinTypes());
+            verify(testList, times(1)).getCoinTypes();
         }
 
         @Test
-        public void getDimeValue() {
-            double dime = Coin.DIME.getCoinValue();
-            double dimeValue = .1;
-            assertEquals(dimeValue, dime);
+        public void testGetDimeValue() {
+            CoinDAOImpl coinDTest = mock(CoinDAOImpl.class);
+            when(coinDTest.getCoinValue()).thenReturn(.1);
+            assertEquals(.1, coinDTest.getCoinValue());
+            verify(coinDTest, times(1)).getCoinValue();
 
         }
 
         @Test
-        public void getCoinInv() {
-            int quarterInv = Coin.QUARTER.getCoinInventory();
-            int numQuarters = 50;
-            assertEquals(numQuarters, quarterInv);
+        public void testGetCoinInv() {
+            Coin quarterInv = Coin.QUARTER;
+            CoinDAOImpl coinDTest = mock(CoinDAOImpl.class);
+            when(coinDTest.getCoinInventory(quarterInv)).thenReturn(50);
+            assertEquals(50, coinDTest.getCoinInventory(quarterInv));
+            verify(coinDTest, times(1)).getCoinInventory(quarterInv);
         }
 
         @Test
-        public void setCoinInv() {
-            Coin.DIME.setCoinInventory(15);
-            int diffInv = Coin.DIME.getCoinInventory();
-            int newInvResults = 15;
-            assertEquals(newInvResults, diffInv);
-        }
+        public void testSetCoinInv() {
+            CoinDAOImpl coinDTest = mock(CoinDAOImpl.class);
+            doNothing().when(coinDTest).setCoinInventory(isA(Integer.class));
+            coinDTest.setCoinInventory(15);
+            verify(coinDTest, times(1)).setCoinInventory(15);
+         }
     }
