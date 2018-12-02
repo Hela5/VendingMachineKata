293cd86262e6b29eba7bbbbf447c14fc641ca8bf
Andria <andria.fuquen@gmail.com>
2018-12-02 16:14:54 -0500

    adding in Mockito to ProductDAOImplTest

--- src/test/java/VendingMachine/ProductDAOImplTest.java
+++ src/test/java/VendingMachine/ProductDAOImplTest.java
@@ -2,43 +2,49 @@ package VendingMachine;
 
 import com.VendingMachine.Product;
 import com.VendingMachine.ProductDAOImpl;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.*;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class ProductDAOImplTest {
     @Test
-    public void canGetAProduct() {
-        Product[] testProduct;
-        testProduct = Product.values();
-        ProductDAOImpl chipTest = new ProductDAOImpl();
-        assertArrayEquals(testProduct, chipTest.getProductTypes());
+    public void testCanGetAProduct() {
+        ProductDAOImpl productDAOTest = mock(ProductDAOImpl.class);
+        Product[] testProduct = Product.values();
+        when(productDAOTest.getProductTypes()).thenReturn(testProduct);
+        assertArrayEquals(testProduct, productDAOTest.getProductTypes());
+        verify(productDAOTest, times(1)).getProductTypes();
     }
 
     @Test
-    public void canGetProductCost() {
-        double chp = Product.CHIPS.getProductCost();
-        double chipsCost = .50;
-        assertEquals(chipsCost, chp);
-
+    public void testCanGetProductCost() {
+        ProductDAOImpl productDAOTest = mock(ProductDAOImpl.class);
+        when(productDAOTest.getProductCost()).thenReturn(.50);
+        assertEquals(.50, productDAOTest.getProductCost());
+        verify(productDAOTest, times(1)).getProductCost();
 
     }
 
     @Test
-    public void canGetInventory(){
-        int candy = Product.CANDY.getProductInventory();
-        int candyCount = 10;
-        assertEquals(candyCount,candy);
+    public void testCanGetInventory(){
+        ProductDAOImpl productDAOTest = mock(ProductDAOImpl.class);
+        Product candy = Product.CANDY;
+        when(productDAOTest.getProductInventory(candy)).thenReturn(10);
+        assertEquals(10,productDAOTest.getProductInventory(candy));
+        verify(productDAOTest, times(1)).getProductInventory(candy);
     }
 
     @Test
-    public void setInventory(){
-        Product.COLA.setProductInventory(7);
-        int diff = Product.COLA.getProductInventory();
-        int newColaInv = 7;
-        assertEquals(newColaInv, diff);
+    public void testSetInventory(){
+        ProductDAOImpl productDAOTest = mock(ProductDAOImpl.class);
+        doNothing().when(productDAOTest).setProductInventory(9);
+        productDAOTest.setProductInventory(9);
+        verify(productDAOTest, times(1 )).setProductInventory(9);
     }
 
 }
